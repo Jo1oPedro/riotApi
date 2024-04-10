4b2eb88ba3c96f9a45846a638b8ba1826624bc29
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-04-10 14:15:22 -0300

    adiciona classe de curl para simplificar requisições

---
+++ src/Helper/Curl.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Riot\Helper;
+
+use stdClass;
+
+class Curl
+{
+    public function request(string $url, string $method, $jsonDecode = true): array|stdClass|string
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_CUSTOMREQUEST => $method
+        ]);
+        $response = ($jsonDecode === true) ? json_decode(curl_exec($curl)) : curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+}
