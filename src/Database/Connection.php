276b6b1457947f16215f606ef5f6831c0f4273b6
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-04-10 14:14:59 -0300

    adiciona class de conexão pdo com sqlite

---
+++ src/Database/Connection.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Riot\Database;
+
+use PDO;
+
+class Connection
+{
+    private static $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ];
+
+    private static ?PDO $instance = null;
+
+    private function __construct() {}
+
+    public static function getInstance(): PDO
+    {
+        if(is_null(self::$instance)) {
+            $databaseDir = __DIR__ . "/../../sqlite/riotApi.db";
+            self::$instance = new PDO("sqlite:{$databaseDir}", options: self::$options);
+        }
+        return self::$instance;
+    }
+}
