02d9263d332b38e186c63eede557bdb7be713556
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-04-19 17:23:05 -0300

    altera interface de httpClient para retornar uma classe de response

--- app/src/Client/HttpClientInterface.php
+++ app/src/Client/HttpClientInterface.php
@@ -7,5 +7,5 @@ use Symfony\Contracts\HttpClient\HttpClientInterface as SymfonyHttpClientInterfa
 interface HttpClientInterface
 {
     public function __construct(SymfonyHttpClientInterface $httpClient);
-    public function request(string $method, string $url): array;
+    public function request(string $method, string $url): Response;
 }
