ab52c5bdc721fe4f2720e4bc64a2554479a949dc
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-04-19 17:21:50 -0300

    adiciona classe de response

---
+++ app/src/Client/Response.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Client;
+
+class Response
+{
+    public function __construct(
+        private string $statusCode,
+        private string $content
+    ) {}
+
+    public function getStatusCode(): string
+    {
+        return $this->statusCode;
+    }
+
+    public function getContent($jsonDecode = true): \stdClass|string
+    {
+        return ($jsonDecode) ? json_decode($this->content) : $this->content;
+    }
+}
