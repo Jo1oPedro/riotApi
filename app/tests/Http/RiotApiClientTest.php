d17b3f4fff0a3f5865aa29ecc51e3ee542a166f0
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-04-22 13:54:45 -0300

    adiciona fixtures aos tests

--- app/tests/Http/RiotApiClientTest.php
+++ app/tests/Http/RiotApiClientTest.php
@@ -23,9 +23,10 @@ class RiotApiClientTest extends TestCase
     #[Test]
     public function should_return_puuid_correctly()
     {
+        $body = file_get_contents(__DIR__ . "/../../helpers/Fixtures/playerPuuid.json");
         $this->httpClient
             ->method("request")
-            ->willReturn(new Response("200", '{"puuid":"123456789","gameName":"Mad Freestyle","tagLine":"BR1"}'));
+            ->willReturn(new Response("200", $body));
 
         $puuid = $this->riotApiClient->get_puuid("Mad Freestyle", "BR1");
 
@@ -35,9 +36,10 @@ class RiotApiClientTest extends TestCase
     #[Test]
     public function should_throw_exception_for_player_not_found()
     {
+        $body = file_get_contents(__DIR__ . "/../../helpers/Fixtures/playerNotFound.json");
         $this->httpClient
             ->method("request")
-            ->willReturn(new Response("404", '{"status":{"status_code":404,"message":"Data not found - No results found for player with riot id Mad Freestyle1#BR1"}}'));
+            ->willReturn(new Response("404", $body));
         $userName = "Mad Freestyle1";
         $tag = "BR1";
 
@@ -49,10 +51,10 @@ class RiotApiClientTest extends TestCase
     #[Test]
     public function should_return_matches_id_correctly()
     {
-        $matchesId = '["BR1_2925730714","BR1_2925713324","BR1_2925688737","BR1_2925255743","BR1_2925233603","BR1_2925216479","BR1_2925198267","BR1_2923547621","BR1_2923259595","BR1_2923247695","BR1_2923181301","BR1_2920235070","BR1_2916654118","BR1_2916629545","BR1_2916396525","BR1_2916380484","BR1_2916365075","BR1_2916156285","BR1_2916141449","BR1_2916119747"]';
+        $body = file_get_contents(__DIR__ . "/../../helpers/Fixtures/matchesId.json");
         $this->httpClient
             ->method("request")
-            ->willReturn(new Response(200, $matchesId));
+            ->willReturn(new Response(200, $body));
 
         $matchesId = $this->riotApiClient->getMatchesId("123456789");
 
